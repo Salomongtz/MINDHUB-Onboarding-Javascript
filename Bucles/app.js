8f9fdc319728b3d6602eacf127b463484af984e6
Salomon Gutierrez <salomon19982009@hotmail.com>
2023-10-30 16:33:30 -0500

    Ejercicio 2.

--- Bucles/app.js
+++ Bucles/app.js
@@ -5,6 +5,18 @@ if (numeroMax >= 1 && numeroMax <= 100) {
     for (let index = numeroMax; index > 0; index--) {
         console.log(index)
     }
-}else{
+} else {
     alert("El número no es válido.")
 }
+
+//Ejercicio 2
+
+const numeroTabla = Number(prompt("Ingrese un número del 1 al 10: "))
+if (numeroTabla >= 1 && numeroTabla <= 10) {
+    for (let index = 1; index <= 10; index++) {
+        console.log(`${index} x ${numeroTabla} = ${index * numeroTabla}`)
+    }
+}
+
+//Ejercicio 3
+
